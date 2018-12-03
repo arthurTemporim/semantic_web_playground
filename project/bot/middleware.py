96cb57a3ef1057683cdb281d8e7984e6f8f4d93d
ArthurTemporim <arthurrtl@gmail.com>
2018-12-02 22:48:16 -0200

    Add more examples to middleware
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- project/bot/middleware.py
+++ project/bot/middleware.py
@@ -1,13 +1,13 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import SPARQLWrapper, JSON, XML, N3
+from rdflib import Graph 
 import simplejson as json
 
 sparql = SPARQLWrapper("https://jena.temposerver.ml/chatbot/query")
 baseQuery = """
-  prefix owl: <http://www.w3.org/2002/07/owl#>
   prefix chatbot: <https://jena.temposerver.ml/chatbot/#>
-  SELECT ?conversation
+  SELECT ?thing
   WHERE {{
-    ?conversation a chatbot:{}
+    ?thing {}
   }}
 """
 
@@ -19,5 +19,10 @@ def get_data(data):
     sparql.setReturnFormat(JSON)
     return sparql.query().convert()
 
-print_json(get_data('ChatBot'))
-print_json(get_data('Conversation'))
+# List bots
+print_json(get_data('a chatbot:ChatBot'))
+# List Conversations
+print_json(get_data('a chatbot:Conversation'))
+# List bots able to talk about something
+print_json(get_data('chatbot:participate chatbot:greet'))
+print_json(get_data('chatbot:participate chatbot:knowUnbRuMenu'))
